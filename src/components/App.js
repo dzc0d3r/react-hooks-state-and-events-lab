21e3bf3559ce3a5728a464fa5bc380bdca478946
dzc0d3r <m4dcod3r@protonmail.com>
2023-09-11 16:53:08 +0100

    Dark / light theme added

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import React from "react"
+import ShoppingList from "./ShoppingList"
+import itemData from "../data/items"
+import {useState} from "react"
 
-function App() {
+function App () {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
   const appClass = false ? "App dark" : "App light"
+  const [darkTheme, setDarkTheme] = useState(false)
 
   return (
-    <div className={appClass}>
+    <div className={darkTheme ? "App dark" : "App light"}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button style={{cursor: "pointer"}} onClick={() => setDarkTheme((darkTheme) => !darkTheme)}>{darkTheme && "Dark Mode" || "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
