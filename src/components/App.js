90eeb0c208d51165beb7137a27c31ff8c8f79e9b
dzc0d3r <m4dcod3r@protonmail.com>
2023-09-11 17:16:05 +0100

    Finished assignement :tada:

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,14 @@ function App () {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  //const appClass = false ? "App dark" : "App light"
   const [darkTheme, setDarkTheme] = useState(false)
 
   return (
     <div className={darkTheme ? "App dark" : "App light"}>
       <header>
         <h2>Shopster</h2>
-        <button style={{cursor: "pointer"}} onClick={() => setDarkTheme((darkTheme) => !darkTheme)}>{darkTheme && "Dark Mode" || "Light Mode"}</button>
+        <button style={{cursor: "pointer"}} onClick={() => setDarkTheme((darkTheme) => !darkTheme)}>{(darkTheme && "Dark Mode") || "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
